b511214c965db7ba9390fc0e4784b44a1bb6481b
Anum7 <anumbhamani@gmail.com>
2017-05-22 13:49:38 -0400

    Add files via upload

---
+++ writeSequence.java
@@ -0,0 +1,51 @@
+package writeSequence;
+public class writeSequence {
+	
+	public static void main(String[] main){
+	writeSequence(1);
+	System.out.println();
+	writeSequence(2);
+	System.out.println();
+	writeSequence(3);
+	System.out.println();
+	writeSequence(4);
+	System.out.println();
+	writeSequence(5);
+	System.out.println();
+	writeSequence(6);
+	System.out.println();
+	writeSequence(7);
+	System.out.println();
+	writeSequence(8);
+	System.out.println();
+	writeSequence(9);
+	System.out.println();
+	writeSequence(10);
+	System.out.println();
+	}
+	public static void writeSequence(int n){
+		
+		if(n<1){
+			return;
+		}
+		if (n==1){
+			System.out.print(n);
+		}else if(n==2){
+			System.out.print(n/2 + " ");
+			System.out.print(n/2 + " ");
+			
+		}else if(n%2==0){
+			System.out.print(n/2 + " ");
+			writeSequence(n-2);
+			System.out.print(n/2 + " ");
+			
+		}else if(n%2 ==1){
+			System.out.print((n/2+1) + " ");
+			writeSequence(n-2);
+			System.out.print(" " + (n/2 + 1));
+		}
+		
+	}
+
+}
+
